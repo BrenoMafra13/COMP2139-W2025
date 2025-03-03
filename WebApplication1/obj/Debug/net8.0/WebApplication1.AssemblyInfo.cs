5c0b99234d47b70d8f1eb70bee7cfc412333d7d8
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-03-03 14:00:24 -0500

    Changes made by lecture class week 9

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1043effe8a9e88ddd601f2d173a3777ad0e53a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84ee6ac8dc92c333da2189f71ebdb5260644e963")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
